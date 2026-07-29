88eb177747e07ef05c5513d35cae4d376cd0134a
Andre-StudioNerd <deco.luisfernandes@gmail.com>
2026-07-29 16:33:25 -0300

    Novo header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,9 +4,16 @@ import { useLocation, useNavigate } from "react-router";
 const Header = () => {
   const navigate = useNavigate();
   const isSignInUp = useLocation().pathname === "/signinup";
+
   return (
-    <div className="flex justify-between items-center p-4">
-      <div className="flex items-center gap-4">
+    <div className="flex justify-between items-center p-4 border-b-2 border-black shadow-md bg-write">
+      {/* Lado Esquerdo - Espaço reservado/Menu */}
+      <div className="w-1/3 flex justify-start">
+        {/* Se tiver ícones/menu no futuro, coloque aqui */}
+      </div>
+
+      {/* Centro - Imagem */}
+      <div className="w-1/3 flex justify-center">
         <img
           className="w-[200px] cursor-pointer"
           src={logo}
@@ -14,6 +21,11 @@ const Header = () => {
           title="Home"
           onClick={() => !isSignInUp && navigate("/")}
         />
+      </div>
+
+      {/* Lado Direito - Espaço reservado/Botões */}
+      <div className="w-1/3 flex justify-end">
+        {/* Se tiver botões de login/perfil, coloque aqui */}
       </div>
     </div>
   );
